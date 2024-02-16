0f3f2959aef8eff41d5369dbe2f1cdd64251eb89
Rehan <rehanalire52@gmail.com>
2024-02-16 21:40:36 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Hero from "./Components/Hero"
+import Navbar from "./Components/Navbar"
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  )
+}
+
+export default App
