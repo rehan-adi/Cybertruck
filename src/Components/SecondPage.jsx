77f2745c3e7b941c211b54c7303f7cd14d951a09
Rehan <rehanalire52@gmail.com>
2024-02-17 18:05:12 +0530

    SecondPage Done

--- src/Components/SecondPage.jsx
+++ src/Components/SecondPage.jsx
@@ -3,15 +3,24 @@ import React from 'react'
 function SecondPage() {
   return (
     <div className='SecondPage-img  h-screen w-auto flex justify-center items-center'>
-        <div className='flex justify-center items-center gap-10'>
-             <div className='w-[10vw] h-[10vh] bg-yellow-300 border-white border-t-2 border-r-2'>
-
+        <div className='flex justify-center mt-[420px] items-center gap-10'>
+             <div className='w-[12vw] h-[12vh] flex justify-center items-center border-white border-t-2 border-r-2'>
+                    <div className='h-[10vh] flex pl-3 justify-center items-start flex-col w-[11vw] bg-[#12121280]'>
+                               <h1 className='text-3xl text-[#C7C7C7]'>11, 000<span className='text-xl ml-5'>LBS</span></h1>
+                               <p className='text-[#A2A3A5] text-sm'>TOWING CAPACITY</p>
+                    </div>
              </div>
-             <div className='w-[10vw] h-[10vh] bg-green-300 border-white border-t-2'>
-
+             <div className='w-[12vw] h-[12vh] flex justify-center items-center border-white border-t-2'>
+             <div className='h-[10vh] flex pl-3 justify-center items-start flex-col w-[11vw] bg-[#12121280]'>
+                      <h1 className='text-3xl text-[#C7C7C7]'>340 <span className='text-xl ml-3'>MILES</span></h1>
+                      <p className='text-[#A2A3A5] text-sm'>EST. RANGE</p>          
              </div>
-             <div className='w-[10vw] h-[10vh] bg-red-700 border-white border-t-2 border-l-2'>
-
+             </div>
+             <div className='w-[12vw] h-[12vh] flex justify-center items-center border-white border-t-2 border-l-2'>
+             <div className='h-[10vh] flex justify-center pl-3 items-start flex-col w-[11vw] bg-[#12121280]'>
+                               <h1 className='text-3xl text-[#C7C7C7]'>2.6<span className='text-xl ml-3'>SEC</span></h1>
+                               <p className='text-[#A2A3A5] text-sm'>0-60MPH</p>
+              </div>
              </div>
         </div>
     </div>
